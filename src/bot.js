db98f35f74f6ba01e2d2cc0a0e3e79560c91eaf0
Joel Parkinson <joelparkinson@live.co.uk>
2019-04-01 23:58:25 +0100

    Swap over to Commando

--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,28 @@
-const { Client } = require('discord.js');
-const client = new Client({ disableEveryone: true });
-const config = require('./Resources/config.json');
+const Commando = require('discord.js-commando');
+const sqlite = require('sqlite');
+const path = require('path');
+const { Token, Prefix } = require('./Resources/config.json');
+
+
+const client = new Commando.Client({
+    owner: '246249703044284426',
+    commandPrefix: Prefix,
+});
+
+client.registry
+    // Registers your custom command groups
+    .registerGroups([
+        ['misc', 'Misc commands'],
+    ])
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.setProvider(
+    sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => new Commando.SQLiteProvider(db))
+).catch(console.error);
 
 client.on('ready', () => {
     console.log('Bot now ready and online.');
 });
 
-client.on('message', msg => {
-    if(!msg.content.startsWith(config.Prefix)) return;
-
-    if(msg.content.startsWith('ping', 1)) {
-        msg.channel.send('pong');
-    }
-});
-
-client.login(config.Token);
+client.login(Token);
